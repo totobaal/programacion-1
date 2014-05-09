56c3732e2ddb41530a8c2bed61bb8c013e49b09e
Cristobal <cinostrozaraos@gmail.com>
2014-05-09 00:18:33 -0400

    snake pantalla finaliza

--- snake.cpp
+++ snake.cpp
@@ -1,9 +1,10 @@
-// comentario demuestra cambios
 #include <ncurses.h>
 #include <time.h>
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
+using namespace std;
+
 void pantalla(int N, int M){
 	for(int i=0;i<N;i++)//filas
         {
@@ -24,17 +25,26 @@ mvprintw(0,30,"Snaaaake !");
 }
 
 void serpiente(int y, int x){
-	
-		
-  mvprintw(y,x,"oooo");
+
+  mvprintw(y,x,"oooooooo");
+  
 }
 
-void comida(){
+void comida(int a, int b){
+	mvprintw(a,b,"X");
+}
+
+void gen_comida(int& a, int& b){
 	srand(time(NULL));
-	int a,b;
+	
 	a = ((rand())%30)+1;
 	b = ((rand())%70)+1;
-	mvprintw(a,b,"x");
+    comida(a,b);	
+}
+
+void score(int N, int M){
+	mvprintw(30,27,"SCORE 00000");
+	
 }
 
 int main()
@@ -43,18 +53,36 @@ int main()
     int y = 10;
     int N = 30;
     int M = 70;
-    int ch; 
+    int ch;
+    int op; 
+ cout<<"==================="<<endl;
+ cout<<"   MENU DE SNAKE   "<<endl;
+ cout<<"==================="<<endl;
+ cout<<"                   "<<endl; 
+ cout<<"1. Jugar"<<endl;
+ cout<<"2. Salir"<<endl;
+ cin>>op;
+  switch(op){
+	  
+	  case 1:
+  
     initscr();
     curs_set(0);
     keypad(stdscr, TRUE);     
-    noecho(); 
-	comida();
+    noecho();
+    
+    int a,b;
+	
+	gen_comida(a,b);
+		
     while (1)
     {
-	
 	    pantalla(N,M); 
+        comida(a,b);
         serpiente(y,x);
-        
+        score(N,M);
+       
+        // verificar que serpiente comio, y generar comida otra vez
         ch = getch();
         if (ch == KEY_DOWN)
         {
@@ -77,10 +105,14 @@ int main()
             clear();
         }
  
-        
 	refresh();
 	  }
     endwin(); 
 
     return 0;
+    break;
+    case 2: 
+    return 0;
+    break;
 }  
+}
